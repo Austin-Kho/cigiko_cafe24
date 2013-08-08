349ee6f18872bec82f8181df951ed2c053a1a30e
cigiko <cigiko@naver.com>
2013-08-08 23:18:05 +0900

    코드이그나이터 연습파일 업데이트

---
+++ ci/bbs/application/models/board_m.php
@@ -0,0 +1,43 @@
+<?php if ( ! defined('BASEPATH')) exit ('No direct script access allowed');
+
+/**
+ * 공통 게시판 모델
+ */
+
+class Board_m extends CI_Model
+{
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	function get_list( $table = 'ci_board', $type='', $offset='', $limit='' )
+	{
+		$limit_query = '';
+
+		if( $limit != '' or $offset != '' )
+		{
+			// 페이징이 있을 경우의 처리
+			$limit_query = ' LIMIT '.$offset.', '.$limit;
+		}
+
+		$sql = "SELECT * FROM ".$table." ORDER BY board_id DESC ".$limit_query;
+		$query = $this->db->query($sql);
+		if( $type == 'count' )
+		{
+			// 리스트를 반환하는 것이 아니라 전체 게시물의 개수를 반환
+			$result = $query->num_rows();
+			// $this->db->count_all($table);
+		}
+		else
+		{
+			// 게시물 리스트 반환
+			$result = $query->result();
+			// $result = $query->result_array();
+		}
+		return $result;
+	}
+}
+
+/* End of file board_m.php */
+/* Location: ./application/models/board_m.php */
