257cf9dbefb00e733d15cb7f5c0623059792a0f4
cigiko <cigiko@naver.com>
2013-08-11 23:01:17 +0900

    ..

--- ci/bbs/application/views/board/list_v.php
+++ ci/bbs/application/views/board/list_v.php
@@ -13,7 +13,7 @@
 			</tr>
 		</thead>
 		<tbody>
-<?php foreach ($list as $lt): ?>
+<?php foreach ($list as $lt){ ?>
 			<tr>
 				<th scope="row"><?php echo $lt->board_id; ?></th>
 				<td><a href="/ci/bbs/<?=$this->uri->segment(1)?>/view/<?=$this->uri->segment(3); ?>/<?=$lt->board_id; ?>" rel="external"><?=$lt->subject; ?></a></td>
@@ -21,7 +21,7 @@
 				<td><?=$lt->hits; ?></td>
 				<td><time datetime="<?=mdate("%Y-%M-%j", $lt->reg_date);?>"><?=mdate("%M, %j,%Y", human_to_unix($lt->reg_date));?></time></td>
 			</tr>
-<?php endforeach; ?>	
+<?php } ?>	
 		</tbody>
 	</table>
 </article>
