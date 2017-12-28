63a6dd1196a23c293015e7401901acbcf9fe577d
Austin-Kho(w) <kori.susie@gmail.com>
2017-12-28 15:40:59 +0900

    cb -> nb 로 재설치

--- ns/application/controllers/Member.php
+++ ns/application/controllers/Member.php
@@ -41,7 +41,7 @@ class Member extends CI_Controller
 	 * @return [type] [description]
 	 */
 	public function login(){
-		// $this->output->enable_profiler(TRUE); //프로파일러 보기//
+		$this->output->enable_profiler(TRUE); //프로파일러 보기//
 
 		// 라이브러리 로드
 		$this->load->library('form_validation'); // 폼 검증
