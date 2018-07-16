dccc08ce242f0bed730690c6ff1d3c0e63dfd35c
austin <kori.susie@gmail.com>
2018-07-17 02:16:19 +0900

    누락된 파일 추가

---
+++ laravel/resources/views/Book02/contents/2.blade.php
@@ -0,0 +1,92 @@
+  <h2 class="page-header">{{$sub[$id][1]}}</h2>
+  <div class="chapter">
+    <section>
+      <article class="">
+        <p>이번 장에서는 파이썬이 무엇인지, 파이썬을 활용하는 법을 배우기 전에 파이썬 그 자체를 알아본다. 파이썬의 역사부터 시작해서 조금은 어려울 수 있는 Hello World 예제까지 해보자.</p>
+      </article>
+    </section>
+  </div>
+
+  <h3 class="sub-header">1.1. 파이썬의 역사</h3>
+  <div class="chapter">
+    <section>
+      <article class="">
+        <p>내용 입력</p>
+      </article>
+    </section>
+  </div>
+
+  <h3 class="sub-header">1.2. 파이썬을 익히면 좋은 점</h3>
+  <div class="chapter">
+    <section>
+      <article class="">
+        <p>내용 입력</p>
+      </article>
+    </section>
+  </div>
+
+  <h3 class="sub-header">1.3. 파이썬 활용이 활발한 개발 분야</h3>
+  <div class="chapter">
+    <section>
+      <article class="">
+        <p>내용 입력</p>
+      </article>
+    </section>
+    <h4 class="sub-header">1.3.1 - 웹 애플리케이션 개발</h4>
+    <section>
+      <article class="">
+        <p>내용 입력</p>
+      </article>
+    </section>
+
+    <h4 class="sub-header">1.3.2 - 크롤링</h4>
+    <section>
+      <article class="">
+        <p>내용 입력</p>
+      </article>
+    </section>
+
+    <h4 class="sub-header">1.3.3 - 데이터 과학과 인공지능 개발</h4>
+    <section>
+      <article class="">
+        <p>내용 입력</p>
+      </article>
+    </section>
+  </div>
+
+  <h3 class="sub-header">1.4. 파이썬 개발 환경 설치</h3>
+  <div class="chapter">
+    <section>
+      <article class="">
+        <p>내용 입력</p>
+      </article>
+    </section>
+
+    <h4 class="sub-header">1.4.1 - 우분투</h4>
+    <section>
+      <article class="">
+        <p>내용 입력</p>
+      </article>
+    </section>
+    <h4 class="sub-header">1.4.2 - macOS</h4>
+    <section>
+      <article class="">
+        <p>내용 입력</p>
+      </article>
+    </section>
+    <h4 class="sub-header">1.4.3 - 윈도우</h4>
+    <section>
+      <article class="">
+        <p>내용 입력</p>
+      </article>
+    </section>
+  </div>
+
+  <h3 class="sub-header">1.5. 조금은 어려운 Hello World</h3>
+  <div class="chapter">
+    <section>
+      <article class="">
+        <p>내용 입력</p>
+      </article>
+    </section>
+  </div>
