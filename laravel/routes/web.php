fc31dc75f63e8787f0767265785c55796e52ec89
austin <kori.susie@gmail.com>
2018-07-23 23:02:30 +0900

    laravel route study

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -23,4 +23,47 @@ Route::get('test/{bookid}/{id?}', 'Book\BookController02@index')->name('book02')
 
 Route::get('book/{bookid}/{id?}', 'Book\BookController@index')->name('book');
 
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| laravel study
+|--------------------------------------------------------------------------
+|
+|
+*/
 Route::get('write', 'WriteController@index')->name('write');
+
+// with 로 view 에 데이터 바인딩
+Route::get('abc', function(){
+    $greeting = 'Hello';
+
+    return view('index')->with('greeting', $greeting);
+});
+
+// with 로 하나 이상의 데이터 바운딩
+Route::get('cde', function(){
+    return view('index')->with([
+        'greeting'=>'Good morning^^/',
+        'name'    => 'Appkr'
+    ]);
+});
+
+// 두번째 인자를 배열로 넘기는 방법
+Route::get('efg', function(){
+    return view('index', [
+        'greeting' => 'Ola~',
+        'name'     => 'Laravelians'
+    ]);
+});
+
+// view 인스턴스의 Property 를 이용하는 방법
+Route::get('ghi', function(){
+    $view = view('index');
+    $view->greeting = "Hey~ What's up";
+    $view->name = 'everyone';
+
+    return $view;
+});
