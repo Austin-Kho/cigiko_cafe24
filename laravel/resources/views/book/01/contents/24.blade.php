735dee7be17089fedfe4bece31d03ddb84deb4c5
austin <kori.susie@gmail.com>
2018-08-02 17:17:07 +0900

    요약 템플릿 추가

--- laravel/resources/views/book/01/contents/24.blade.php
+++ laravel/resources/views/book/01/contents/24.blade.php
@@ -12,4 +12,14 @@
         </ul>
       </article>
     </section>
+
+    <h3 class="sub-header"><svg id="i-file" viewBox="0 0 32 32" width="20" height="20" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+      <path d="M6 2 L6 30 26 30 26 10 18 2 Z M18 2 L18 10 26 10" />
+      </svg> 요약
+    </h3>
+    <section>
+      <article>
+        <p>내용 입력</p>
+      </article>
+    </section>
   </div>
